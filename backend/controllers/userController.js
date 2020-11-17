27893ac350265bb4e69b5c8982833532901b642d
Chris Owens <guitarrockstarco@gmail.com>
2020-11-17 09:15:48 -0500

    Product List Reducer and Action

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -9,18 +9,21 @@ const authUser = asyncHandler(async (req, res) => {
     const { email, password } = req.body;
 
     const user = await User.findOne({ email });
-
-    if (user && (await user.matchPassword(password))) {
-        res.json({
-            _id: user._id,
-            name: user.name,
-            email: user.email,
-            isAdmin: user.isAdmin,
-            token: generateToken(user._id),
-        });
-    } else {
-        res.status(401);
-        throw new Error('Invalid email or password');
+    try {
+        if (user && (await user.matchPassword(password))) {
+            res.json({
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                isAdmin: user.isAdmin,
+                token: generateToken(user._id),
+            });
+        } else {
+            res.status(401);
+            throw new Error('Invalid email or password');
+        }
+    } catch (error) {
+        console.error(error);
     }
 });
 
@@ -52,7 +55,7 @@ const registerUser = asyncHandler(async (req, res) => {
         });
     } else {
         res.status(400);
-        throw new Error('Invalis user data');
+        throw new Error('Invalid user data');
     }
 });
 
