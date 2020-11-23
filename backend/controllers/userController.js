e49ff37e1edbefc02a14dff59f1789110fc21c8c
Chris Owens <guitarrockstarco@gmail.com>
2020-11-22 19:50:37 -0500

    Remove try catch from authuser

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -9,21 +9,17 @@ const authUser = asyncHandler(async (req, res) => {
     const { email, password } = req.body;
 
     const user = await User.findOne({ email });
-    try {
-        if (user && (await user.matchPassword(password))) {
-            res.json({
-                _id: user._id,
-                name: user.name,
-                email: user.email,
-                isAdmin: user.isAdmin,
-                token: generateToken(user._id),
-            });
-        } else {
-            res.status(401);
-            throw new Error('Invalid email or password');
-        }
-    } catch (error) {
-        console.error(error);
+    if (user && (await user.matchPassword(password))) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id),
+        });
+    } else {
+        res.status(401);
+        throw new Error('Invalid email or password');
     }
 });
 
