4c1ab25c70de786b21c404a801f58a649290e65c
Chris Owens <guitarrockstarco@gmail.com>
2020-11-25 14:07:10 -0500

    Mark order as delivered

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -59,7 +59,7 @@ const orderSchema = mongoose.Schema(
         paidAt: {
             type: Date,
         },
-        idDelivered: {
+        isDelivered: {
             type: Boolean,
             required: true,
             default: false,
