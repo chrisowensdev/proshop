b4b3c08854f5fb36cfdcc6ad8131b8ed75cae57c
Chris Owens <guitarrockstarco@gmail.com>
2020-11-25 10:20:51 -0500

    Admin Create Product

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,6 +5,7 @@ import {
     productListReducer,
     productDetailsReducer,
     productDeleleReducer,
+    productCreateReducer,
 } from './reducers/productReducers';
 import { cartReducer } from './reducers/cartReducers';
 import {
@@ -27,6 +28,7 @@ const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
     productDelete: productDeleleReducer,
+    productCreate: productCreateReducer,
     cart: cartReducer,
     userLogin: userLoginReducer,
     userRegister: userRegisterReducer,
